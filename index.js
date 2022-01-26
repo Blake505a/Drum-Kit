630323536f141bed778508cd2070029b70261493
Jose Vladimir Sosa <61264026+Blake505a@users.noreply.github.com>
2022-01-26 11:10:54 -0500

    Update index.js
    add cap-lock functionality

--- index.js
+++ index.js
@@ -20,37 +20,45 @@ document.addEventListener("keydown", function(event) {
 function makesound(key) {
 
   switch (key) {
+      
     case "w":
+    case "W":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "a":
+    case "A":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
     case "s":
+    case "S":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
     case "d":
+    case "D":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
     case "j":
+    case "J":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
     case "k":
+    case "K":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
     case "l":
+    case "L":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
